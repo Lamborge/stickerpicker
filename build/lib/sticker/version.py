c3c84f7eb30165642f1ef3454d70d51b9b01fb18
Lamborge <maxpopelov@gmail.com>
2023-10-23 23:26:45 +0300

    add cats stickerpack

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f59406a4'
-version = '0.1.0+dev.f59406a4'
-linkified_version = '0.1.0+dev.[f59406a4](https://github.com/maunium/stickerpicker/commit/f59406a47a6778cd402e656ffb64f667335f665a)'
+git_revision = 'a8670fc5'
+version = '0.1.0+dev.a8670fc5'
+linkified_version = '0.1.0+dev.[a8670fc5](https://github.com/maunium/stickerpicker/commit/a8670fc58151eeb2a8e8547f7b2a002dd12bf4d2)'
