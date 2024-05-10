553e3df10b17df21d3fa42d3050c2a8ebea4f96b
Lamborge <maxpopelov@gmail.com>
2024-05-10 18:01:57 +0300

    added stupidest7

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '7c5d8b10'
-version = '0.1.0+dev.7c5d8b10'
-linkified_version = '0.1.0+dev.[7c5d8b10](https://github.com/maunium/stickerpicker/commit/7c5d8b105dbb1b6fd486bd114725c23871d05a9b)'
+git_revision = 'a7c01b49'
+version = '0.1.0+dev.a7c01b49'
+linkified_version = '0.1.0+dev.[a7c01b49](https://github.com/maunium/stickerpicker/commit/a7c01b49aaf36f821da5838d9a334018544899f8)'
